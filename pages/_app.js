465f61bebc43c46bb0c5490e8bbee75e50307a5b
João Eduardo <joaoeduardo980@gmail.com>
2021-01-25 22:36:46 -0300

    Aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,50 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
+
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* New styles */
+  display: flex;
+  flex-direction: column;
+  font-family: 'Lato', sans-serif;
+  // Deixa branco no começo
+  color: ${({ theme }) => theme.colors.contrastText};
+}
+html, body {
+  min-height: 100vh;
+}
+#__next {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
+
+// const theme = {
+//   colors: {
+//     primary: '#0070f3',
+//   },
+// }
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      
       <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        {/* o component muda para cada página 
+        ele moveu o global style, para dentro do theme
+        provider, pois desse modo vai funcionar tudo que 
+        referência o tema, pois estará dentro do theme*/}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget/index'
+import Footer from '../src/components/Footer/index'
+import GitHubCorner from '../src/components/GitHubCorner/index'
+import QuizBackground from '../src/components/QuizBackground/index'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+// function Title(props){ //props
+// return <h1>{props.children}</h1>
+// }
+
+//div com o backgound
+
+
+// export default function Home() {
+//   return <Title>My page</Title>
+// }
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex:1;
+//   background-size:cover;
+//   background-position:center;
+// `
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>adasdasdfahadhasdhasdh</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>asdasdadasddsa</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>adasdasdfahadhasdhasdh</h1>
+            <p>asdasdadasddsa</p>
+          </Widget.Content>
+        </Widget>
+        <Footer>
+
+        </Footer>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/joaoeduardodev"/>
+    </QuizBackground>
+  );
 }
